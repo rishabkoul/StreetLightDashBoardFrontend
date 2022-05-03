1a8c53ff3aecaa7a0be046e5194a8a6bf482a73d
Rishab Koul <rishabkoul2001@gmail.com>
2022-05-04 02:58:09 +0530

    initial-commit

---
+++ src/components/StreetLightTable/StreetLightTable.js
@@ -0,0 +1,123 @@
+import { useState, useEffect } from "react";
+import styles from "./StreetLightTable.css";
+
+const StreetLightTable = () => {
+  const [data, setData] = useState();
+  const [pageno, setPageNo] = useState(1);
+  const [gotopage, setGoToPage] = useState();
+
+  const getData = async () => {
+    const response = await fetch(
+      `https://streetlightdashboardbackend.herokuapp.com/api/get_all?no_of_results_per_page=10&page_no=${pageno}`
+    );
+    setData(await response.json());
+  };
+
+  const goToPage = (e) => {
+    e.preventDefault();
+    if (!gotopage) {
+      return alert("Enter a Page to Visit");
+    }
+    if (gotopage < 1) {
+      return alert("Page cannot be less then One");
+    }
+    if (gotopage > parseInt(data[0]["no_of_pages"])) {
+      return alert("Page cannot be greater than the Last Page");
+    }
+    if (pageno !== gotopage) {
+      setData(null);
+      setPageNo(gotopage);
+    }
+  };
+  useEffect(() => {
+    // Update the document title using the browser API
+    getData();
+  }, [pageno]);
+
+  return (
+    <div>
+      {data ? (
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>BV</th>
+                <th>BI</th>
+                <th>SV</th>
+                <th>SI</th>
+                <th>LV</th>
+                <th>LI</th>
+                <th>BA</th>
+                <th>STATE</th>
+                <th>LAT</th>
+                <th>LON</th>
+                <th>DRY_BIN</th>
+                <th>WET_BIN</th>
+                <th>TIME_STAMP</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data[0]["Streetlights"].map((streetlight) => {
+                return (
+                  <tr key={streetlight.ID}>
+                    <td>{streetlight.ID}</td>
+                    <td>{streetlight.BV}</td>
+                    <td>{streetlight.BI}</td>
+                    <td>{streetlight.SV}</td>
+                    <td>{streetlight.SI}</td>
+                    <td>{streetlight.LV}</td>
+                    <td>{streetlight.LI}</td>
+                    <td>{streetlight.BA}</td>
+                    <td>{streetlight.STATE}</td>
+                    <td>{streetlight.LAT}</td>
+                    <td>{streetlight.LON}</td>
+                    <td>{streetlight.DRY_BIN}</td>
+                    <td>{streetlight.WET_BIN}</td>
+                    <td>{streetlight.TIME_STAMP}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <p>
+            Page No: {pageno} of {data[0]["no_of_pages"]}
+          </p>
+          <button
+            onClick={() => {
+              if (pageno !== 1) {
+                setData(null);
+                setPageNo((prev) => prev - 1);
+              }
+            }}
+          >
+            Previous Page
+          </button>
+          <button
+            onClick={() => {
+              const no_of_pages = parseInt(data[0]["no_of_pages"]);
+              if (pageno !== no_of_pages) {
+                setData(null);
+                setPageNo((prev) => prev + 1);
+              }
+            }}
+          >
+            Next Page
+          </button>
+          <form onSubmit={goToPage}>
+            <input
+              onChange={(e) => setGoToPage(parseInt(e.target.value))}
+              type="number"
+              placeholder="Enter Page no to Visit"
+            ></input>
+            <button type="submit">Go</button>
+          </form>
+        </div>
+      ) : (
+        <h1>Loading ...</h1>
+      )}
+    </div>
+  );
+};
+
+export default StreetLightTable;
