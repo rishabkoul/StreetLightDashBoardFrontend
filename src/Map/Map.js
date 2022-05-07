6d2e0c715292152e6a2adbecda8f8f2f81e73f73
Rishab Koul <rishabkoul2001@gmail.com>
2022-05-07 20:33:50 +0530

    added-map

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,49 +1,49 @@
-import React, { useRef, useState, useEffect } from "react"
+import React, { useRef, useState, useEffect } from "react";
 import "./Map.css";
 import MapContext from "./MapContext";
 import * as ol from "ol";
 
 const Map = ({ children, zoom, center }) => {
-	const mapRef = useRef();
-	const [map, setMap] = useState(null);
+  const mapRef = useRef();
+  const [map, setMap] = useState(null);
 
-	// on component mount
-	useEffect(() => {
-		let options = {
-			view: new ol.View({ zoom, center }),
-			layers: [],
-			controls: [],
-			overlays: []
-		};
+  // on component mount
+  useEffect(() => {
+    let options = {
+      view: new ol.View({ zoom, center }),
+      layers: [],
+      controls: [],
+      overlays: [],
+    };
 
-		let mapObject = new ol.Map(options);
-		mapObject.setTarget(mapRef.current);
-		setMap(mapObject);
+    let mapObject = new ol.Map(options);
+    mapObject.setTarget(mapRef.current);
+    setMap(mapObject);
 
-		return () => mapObject.setTarget(undefined);
-	}, []);
+    return () => mapObject.setTarget(undefined);
+  }, [center, zoom]);
 
-	// zoom change handler
-	useEffect(() => {
-		if (!map) return;
+  // zoom change handler
+  useEffect(() => {
+    if (!map) return;
 
-		map.getView().setZoom(zoom);
-	}, [zoom]);
+    map.getView().setZoom(zoom);
+  }, [zoom, map]);
 
-	// center change handler
-	useEffect(() => {
-		if (!map) return;
+  // center change handler
+  useEffect(() => {
+    if (!map) return;
 
-		map.getView().setCenter(center)
-	}, [center])
+    map.getView().setCenter(center);
+  }, [center, map]);
 
-	return (
-		<MapContext.Provider value={{ map }}>
-			<div ref={mapRef} className="ol-map">
-				{children}
-			</div>
-		</MapContext.Provider>
-	)
-}
+  return (
+    <MapContext.Provider value={{ map }}>
+      <div ref={mapRef} className="ol-map">
+        {children}
+      </div>
+    </MapContext.Provider>
+  );
+};
 
 export default Map;
